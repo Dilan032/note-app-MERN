dd162a38852dbe77b540e4f2e9f3327ca0283ebe
Dilan kanishka <dilankanishka032@gmail.com>
2025-09-13 08:10:57 +0530

    develop Signup font-end

--- font-end/src/App.jsx
+++ font-end/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Signup } from "./pages/Signup"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='text-3xl font-bold underline'>
-        hello
-      </p>
+      {/* <p className='text-4xl font-bold'>
+        Note app
+      </p> */}
+      <Signup />
     </>
   )
 }
