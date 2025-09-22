adbc782eb233b4258a42cef0a379eb98e5ab67b9
Dilan kanishka <dilankanishka032@gmail.com>
2025-09-22 16:48:14 +0530

    add env file

--- back-end/database/db.js
+++ back-end/database/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connecToMongoDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://dilan:dilan@cluster0.3yjectp.mongodb.net/noteApp");
+        await mongoose.connect(process.env.MONGO_DB_URL);
         console.log("Connect to MongoDB");
         
     } catch (error) {
